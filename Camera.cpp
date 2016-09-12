ac745e26fbda328e64d363fc4dc1f68ed0f22e26
Ava-chan <d56rknvGIT>
2016-09-12 13:40:52 -0700

    first commit

---
+++ Camera.cpp
@@ -0,0 +1,158 @@
+#include <fantom/algorithm.hpp>
+#include <fantom/register.hpp>
+#include <fantom/graphics.hpp>
+#include <fantom/fields.hpp>
+#include <fantom/outputs/VisOutputs.hpp>
+
+
+using namespace fantom;
+
+namespace {
+
+    class CameraAlgorithm : public VisAlgorithm {
+        // Zeiger auf interne Grafikobjekte
+        //std::unique_ptr<Primitive> mPoints;
+        std::unique_ptr<Primitive> mTriangle;
+
+    public:
+        // Ausgaben der Visualisierung bekanntmachen
+        struct VisOutputs : public VisAlgorithm::VisOutputs {
+            VisOutputs(fantom::VisOutputs::Control &control)
+                    : VisAlgorithm::VisOutputs(control) {
+                // Zwei benannte Grafikobjekte
+                //addGraphics("points");
+                addGraphics("triangle");
+            }
+        };
+
+        struct Options
+                : public DataAlgorithm::Options {
+        public:
+            Options(Algorithm::Options::Control &control)
+                    : DataAlgorithm::Options(control) {
+                // Ein Feld mit Werten
+                add<TensorFieldDiscrete<Scalar> >("field", "");
+            }
+        };
+
+
+        CameraAlgorithm(InitData &data)
+                : VisAlgorithm(data) {
+        }
+
+        virtual void execute(const Algorithm::Options &options, const volatile bool & abortFlag ) override {
+            // loeschen der aktuellen Grafik und anlegen neuer Grafikobjekte
+            //mPoints.reset();
+            //mPoints = getGraphics("points").makePrimitive();
+            mTriangle.reset();
+            mTriangle = getGraphics("triangle").makePrimitive();
+
+            // Lesen der parameter
+            auto field = options.get<const TensorFieldDiscrete<Scalar> >("field");
+
+            //debugLog() << "exec" << std::endl;
+            // Testen welche Eingaben gesetzt sind
+            if (field) {
+                //debugLog() << "field with values" << std::endl;
+                // Punkte + Werte?
+                // Zeige beides an
+                auto domain = std::dynamic_pointer_cast<const DiscreteDomain<2> >(field->domain());
+
+                auto eval = field->makeDiscreteEvaluator();
+                std::vector<Point3> tri(3);
+                if (domain) {
+                    int width = 512, height = 424;
+                    int stepsize = 1;
+                    int eps = 10;
+                    float colorrange = 4096.0;
+
+                    bool first;
+                    int pitch;
+                    int k;
+
+                    for (int h = 0; h < height - stepsize && !abortFlag; h+=stepsize) {
+                        first = true;
+                        std::vector<Color> colorTriangle(3, Color(1.0, 1.0, 1.0, 1.0));
+                        for (int w = 0; w < width - stepsize && !abortFlag; w+=stepsize) {
+                            size_t i = h * width + w;
+                            if (field->values()[i][0] != 0) {
+                                int newpitch = field->values()[i + stepsize][0] - field->values()[i][0];
+
+                                if (!first) {
+                                    if (newpitch > pitch - eps && newpitch < pitch +eps) {
+                                        tri[1] = toPoint3(domain->points()[i + stepsize]);
+                                        pitch = pitch + (newpitch - pitch) / k;
+                                        float color = (float)(field->values()[i + stepsize][0]) / colorrange;
+
+                                        colorTriangle[1] = Color(color, 1.0, 1.0, 1.0);
+                                        k++;
+                                    } else {
+                                        /*float color = (float)field->values()[i][0] / 4096.0;
+
+                                        std::vector<Color> colorTriangle(3, Color(color, color, color, 1.0));*/
+
+                                        mTriangle->add(Primitive::TRIANGLE_STRIP)
+                                                .setLineWidth(1.0)
+                                                .setColors(colorTriangle)
+                                                .setVertices(tri);
+                                        first = true;
+                                    }
+                                }
+                                if (first) {
+                                    tri[0] = toPoint3(domain->points()[i]);
+                                    tri[0][2] = (float)field->values()[i][0] / colorrange;
+                                    float color = (float)(field->values()[i][0]) / colorrange;
+                                    colorTriangle[0] = Color(1.0, color, color);
+
+                                    tri[1] = toPoint3(domain->points()[i + stepsize]);
+                                    tri[1][2] = (float)field->values()[i + stepsize][0] / colorrange;
+                                    color = ((float)field->values()[i + stepsize][0]) / colorrange;
+                                    colorTriangle[1] = Color(1.0, color, color);
+
+                                    tri[2] = toPoint3(domain->points()[i + stepsize * width]);
+                                    tri[2][2] = (float)field->values()[i + stepsize * width][0] / colorrange;
+                                    color = ((float)field->values()[i + stepsize * width][0]) / colorrange;
+                                    colorTriangle[2] = Color(1.0, color, color);
+                                    first = false;
+                                    pitch = newpitch;
+                                    k = 1;
+                                }
+                            }
+                        }
+                    }
+                    /*for (size_t i = 0; i < domain->numPoints() && !abortFlag; i=i+stepsize) {
+                        //Temperatur wird zwischen -20 und +40 °C in Farbe gewandelt
+
+                        //der letzte aus jeder row nicht
+                        if ((i+1) % width != 0 ) {
+                            //der letzte am rand nicht
+                            if ((int)i+width < (int)domain->numPoints()) {
+
+
+                                tri[1] = toPoint3(domain->points()[i+stepsize]);
+
+                                tri[0] = toPoint3(domain->points()[i]);
+                                tri[2] = toPoint3(domain->points()[i+stepwidth]);
+
+                                float color = (float)field->values()[i][0] / 4096.0;
+                                infoLog() << " x " << domain->points()[i][0] << " y " << domain->points()[i][1] << " - " << color << std::endl;
+                                std::vector<Color> colorTriangle(3, Color(color, color, color, 1.0));
+                                colorTriangle[0] = Color(color, color, color);
+                                colorTriangle[1] = Color(color, color, color);
+                                colorTriangle[2] = Color(color, color, color);
+
+                                mTriangle->add(Primitive::TRIANGLE_STRIP)
+                                        .setLineWidth(1.0)
+                                        .setColors(colorTriangle)
+                                        .setVertices(tri);
+                            }
+                        }
+                    }*/
+                }
+            }
+        }
+    };
+
+    AlgorithmRegister<CameraAlgorithm> dummy("Camera", "Do the Camerastuff");
+} // namespace
+
---
+++ Wall.cpp
@@ -0,0 +1,124 @@
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+#include "lodepng.h"
+
+#include <fantom/algorithm.hpp>
+#include <fantom/fields.hpp>
+#include <fantom/register.hpp>
+using namespace fantom;
+
+
+namespace
+{
+
+    /**
+    * Loader for a simple tabular format with Geo/Meteo information
+    * Each entry is a line
+    * Each line contains (at least) the following data
+    * latitude longitude Temperature
+    * The first two lines are comment lines and are ignored
+    */
+    class LoadWallAlgorithm : public DataAlgorithm
+    {
+    public:
+
+        struct Options : public DataAlgorithm::Options
+        {
+        public:
+            Options(Algorithm::Options::Control& control) : DataAlgorithm::Options(control)
+            {
+                add<InputLoadPath>("Input depth file", "The file to be read", "");
+                add<InputLoadPath>("Input color file", "The file to be read", "");
+            }
+        };
+
+
+        struct DataOutputs : public DataAlgorithm::DataOutputs
+        {
+        public:
+            DataOutputs( Control& control ) : DataAlgorithm::DataOutputs( control )
+            {
+                add<DomainBase>("Points");
+                add<TensorFieldBase>("depth");
+                add<TensorFieldBase>("color");
+            }
+        };
+
+
+        LoadWallAlgorithm( InitData& data ) : DataAlgorithm( data )
+        {
+        }
+
+
+        void execute(const Algorithm::Options& parameters, const volatile bool& abortFlag)
+        {
+            // Test, ob die Dati gesetzt ist, sonst nichts machen
+            if (parameters.get<std::string>("Input depth file") != "")
+            {
+                std::string filename = parameters.get<std::string>("Input depth file");
+
+                // Speicher fuer die Daten
+                std::vector<Point2> positions;
+                std::vector<Scalar> depth;
+
+                // Schleife zum einlesen der Daten
+                std::vector<unsigned char> image; //the raw pixels
+                unsigned width, height;
+
+                //decode
+                unsigned error = lodepng::decode(image, width, height, filename);
+
+                //if there's an error, display it
+                if(error) infoLog() << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+
+                infoLog() << "loaded picture with width " << width << " and height " << height << std::endl;
+                int pointcount = 0;
+
+                for (unsigned h = 0; h < height && !abortFlag; h++) {
+                    for (unsigned w = 0; w < width && !abortFlag; w++) {
+                    //infoLog() << "w " << w << " - gray value " << ((((int)image.at(w * 4 + 0)) + ((int)(image.at(w * 4 + 1)) << 8)) >> 4) << std::endl;
+                        int dep = ((((int)image.at((w + h * width) * 4 + 0)) + ((uint8_t)(image.at((w + h * width) * 4 + 1)) << 8)) >> 4);
+                        //if (dep != 0) {
+                            positions.push_back(Point2(h, w));
+                            depth.push_back(Scalar(dep));
+                            pointcount++;
+                        //}
+                    }
+                }
+
+                infoLog() << "loaded " << pointcount << " points " << std::endl;
+
+
+
+                auto domain = DomainFactory::makeDomainArbitrary(positions);
+                auto fieldDepth  = DomainFactory::makeTensorField(*domain, depth);
+
+                setResult("Points", domain);
+                setResult("depth", fieldDepth);
+            }
+        }
+
+        IMPLEMENT_CAN_HANDLE(".png")
+
+        static bool loaderSetOptions(Algorithm::Options& options, std::vector<std::string>& filenames)
+        {
+            // find the name we will handle
+            for (auto it = filenames.begin(); it != filenames.end(); ++it)
+            {
+                if (it->substr(it->size()-4, 4) == ".png")
+                {
+                    options.set<std::string>("Input depth file", *it);
+                    it = filenames.erase(it);
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+
+    AlgorithmRegister<LoadWallAlgorithm> dummy("Wall", "Load depth and color informations for a wall representation");
+    DataLoaderRegister<LoadWallAlgorithm> dummy2("Wall");
+} // namespace
